adb13aff46cad3cdf242c1e41eb51b9c5a7a5046
Shivraj Chvada <shivraj@webtheunivesre.com>
2022-09-06 17:59:09 +0530

    Task Updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import { useState } from 'react';
+import axios from 'axios';
+import React, { useRef } from 'react'
+import { useState, useEffect } from 'react';
 import { Food } from './Food';
 
 export const NavBar = () => {
-
-    const [targetValue, setTargetValue] = useState(0)
+    
+    const [targetValue, setTargetValue] = useState('burgers')
 
     return (
         <div className='main-container'>
             <nav className='navbar'>
                 <ul className='flex'>
-                    <li><button onClick={(e) => setTargetValue(e.target.value)}
-                    className={targetValue === 'burgers' ? 'active-btn' : ''}
-                    value='burgers'>Burgers</button></li>
-                    <li><button onClick={(e) => setTargetValue(e.target.value)} 
-                    className={targetValue === 'pizzas' ? 'active-btn' : ''}
-                    value='pizzas'>Pizzas</button></li>
-                    <li><button onClick={(e) => setTargetValue(e.target.value)}
-                    className={targetValue === 'sandwiches' ? 'active-btn' : ''}
-                    value='sandwiches'>Sandwiches</button></li>
-                    <li><button onClick={(e) => setTargetValue(e.target.value)} 
-                    className={targetValue === 'chocolates' ? 'active-btn' : ''}
-                    value='chocolates'>Chocolates</button></li>
+                    <li>
+                        <button
+                            onClick={(e) => setTargetValue(e.target.value)}
+                            className={targetValue === 'burgers' ? 'active-btn' : ''}
+                            value='burgers'>Burgers</button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={(e) => setTargetValue(e.target.value)}
+                            className={targetValue === 'pizzas' ? 'active-btn' : ''}
+                            value='pizzas'>Pizzas</button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={(e) => setTargetValue(e.target.value)}
+                            className={targetValue === 'sandwiches' ? 'active-btn' : ''}
+                            value='sandwiches'>Sandwiches</button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={(e) => setTargetValue(e.target.value)}
+                            className={targetValue === 'chocolates' ? 'active-btn' : ''}
+                            value='chocolates'>Chocolates</button>
+                    </li>
                 </ul>
             </nav>
             <hr className='header-hr' />
-            {
-                targetValue ? <Food foodName={targetValue}/> : ''
-            }
+            {/* {loading ? <PageLoader /> : initial ? <Food foodName='burgers' /> : ''} */}
+            {targetValue ? <Food foodName={targetValue} /> : ''}
         </div>
     )
 }
--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const PageLoader = () => {
   return (
-    <span class="loader"></span>
+    <span className="loader"></span>
   )
 }
