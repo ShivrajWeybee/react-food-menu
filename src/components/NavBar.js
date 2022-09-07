fdaddcaf8d9aea59fe124bfabaa89aabcc924e3a
Shivraj Chvada <shivraj@webtheunivesre.com>
2022-09-07 11:58:55 +0530

    Loader Updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import React, { Suspense } from 'react'
 import { useState } from 'react';
 import { Food } from './Food';
 
 export const NavBar = () => {
 
-=======
-import axios from 'axios';
-import React, { useRef } from 'react'
-import { useState, useEffect } from 'react';
-import { Food } from './Food';
-
-export const NavBar = () => {
-    
->>>>>>> adb13aff46cad3cdf242c1e41eb51b9c5a7a5046
     const [targetValue, setTargetValue] = useState('burgers')
 
     return (
@@ -47,10 +37,6 @@ export const NavBar = () => {
                 </ul>
             </nav>
             <hr className='header-hr' />
-<<<<<<< HEAD
-=======
-            {/* {loading ? <PageLoader /> : initial ? <Food foodName='burgers' /> : ''} */}
->>>>>>> adb13aff46cad3cdf242c1e41eb51b9c5a7a5046
             {targetValue ? <Food foodName={targetValue} /> : ''}
         </div>
     )
